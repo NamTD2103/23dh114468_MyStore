4a60f84fd0be253a3602650cc57b871efd842d88
SV <SV@PM19-PC04>
2024-10-14 09:01:31 +0700

    Add project files.

---
+++ 23dh114468_MyStore/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _23dh114468_MyStore.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/HOme
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
